ec15b01497c11059dd44f3ffd4ad8a5f4fa0fe31
artemoska <medvedevaelena464@gmail.com>
2024-12-01 20:48:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-<script>
-    var audio = document.getElementById('music');
-    audio.play().then(function() {
-        console.log('Music is playing!');
-    }).catch(function(error) {
-        console.log('Error playing music:', error);
-    });
-</script>
+// Пример правильного содержимого файла script.js
+var audio = document.getElementById('music');
+audio.play().then(function() {
+    console.log('Music is playing!');
+}).catch(function(error) {
+    console.log('Error playing music:', error);
+});
