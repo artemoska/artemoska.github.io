8df9a387cec615e672cad06605ec36433471fdca
artemoska <medvedevaelena464@gmail.com>
2024-12-01 20:44:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+<script>
+    var audio = document.getElementById('music');
+    audio.play().then(function() {
+        console.log('Music is playing!');
+    }).catch(function(error) {
+        console.log('Error playing music:', error);
+    });
+</script>
